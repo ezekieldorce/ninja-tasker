575aba6eee8e9d509fd5a5b1b5a418454e0330d7
Ezekiel <ezekieldorce@gmail.com>
2019-11-12 16:30:31 -0500

    Fixed server problems, now runs smoothly

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-function 
+//packages
+const express = require('express');
+
+//starting express app
+const app = express();
+
+//Setting view engine
+app.set("view engine", "ejs");
+
+//middleware
+const bodyParser = require('body-parser');
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+let list = ["watch anime", "Slackline tonight"];
+
+//ROUTESfunction
+app.get("/home", function(req, res) {
+    res.render("home.html", {list: list})
+});
 
 
-something()
+//POST /ninja
+app.post("/ninja,", function(req, res){
+    console.log(req.body.taskItem);
+    list.push(req.body.taskItem);
+    res.render("home.ejs", {})
+});
 
-
-
-{
-
-}
+app.listen(3000, function(){
+    console.log('Server is live on port: 3000');
+});
